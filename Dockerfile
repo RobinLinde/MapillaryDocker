d82801270a8aea6ce41393419d83d092aa571188
Robin van der Linde <robin.vanderlinde@gmail.com>
2020-01-18 23:38:24 +0100

    Basic Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:alpine
+MAINTAINER Robin van der Linde (robin.vanderlinde@gmail.com
+COPY install.sh .
+RUN ./install.sh
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apk add --no-cache git ffmpeg
+pip install git+https://github.com/mapillary/Piexif
+pip install --upgrade git+https://github.com/mapillary/mapillary_tools
